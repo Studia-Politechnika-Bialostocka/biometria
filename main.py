a63892b61d75164bac92035ebdf8fc73aba17d98
mateuszbialowas <mateusz.bialowas2@gmail.com>
2022-03-13 17:38:37 +0100

    .

--- main.py
+++ main.py
@@ -1,16 +1,73 @@
-# This is a sample Python script.
+from tkinter import *
+from PIL import ImageTk, Image
+from tkinter import filedialog
+import pathlib
+import pygubu
+import tkinter as tk
+import tkinter.ttk as ttk
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+PROJECT_PATH = pathlib.Path(__file__).parent
+PROJECT_UI = PROJECT_PATH / "biometria_gui.ui"
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class BiometriaGuiApp:
+    def __init__(self, master=None):
+        # build ui
+        self.main_window = tk.Tk() if master is None else tk.Toplevel(master)
+        self.menu_frame = tk.Frame(self.main_window)
+        self.select_image_button = tk.Button(self.menu_frame)
+        self.select_image_button.configure(cursor='hand2', text='Select Image')
+        self.select_image_button.pack(ipadx='10', pady='10', side='top')
+        self.select_image_button.configure(command=self.select_and_display_image)
+        self.menu_frame.configure(background='#0d938c', height='800', width='200')
+        self.menu_frame.pack(side='left')
+        self.menu_frame.pack_propagate(0)
+        self.main_frame = tk.Frame(self.main_window)
+        self.original_image_canvas = tk.Canvas(self.main_frame)
+        self.original_image_canvas.configure(background='#0e5092', cursor='hand2', height='400', width='400')
+        self.original_image_canvas.pack(anchor='center', side='left')
+        self.changed_image_1_canvas = tk.Canvas(self.main_frame)
+        self.changed_image_1_canvas.configure(background='#970925', cursor='hand2', height='400', width='400')
+        self.changed_image_1_canvas.pack(anchor='center', side='right')
+        self.main_frame.configure(background='#ff80c0', height='800', width='1166')
+        self.main_frame.pack(side='top')
+        self.main_frame.pack_propagate(0)
+        self.main_window.configure(height='200', width='200')
+        self.main_window.geometry('1366x768')
+        self.main_window.resizable(False, False)
+        self.main_window.title('Biometric Basics')
+
+        # Main widget
+        self.mainwindow = self.main_window
+
+    def run(self):
+        self.mainwindow.mainloop()
+
+    def select_and_display_image(self):
+        filename = filedialog.askopenfilename(title='Select an image')
+        img = Image.open(filename)
+        img = self.change_image_size(img)
+        self.display_original_image(img)
+
+    def display_original_image(self, img):
+        # PhotoImage class is used to add image to widgets, icons etc
+        img = ImageTk.PhotoImage(img)
+        self.original_image_canvas.create_image(0, 0, image=img, anchor='nw')
+        self.original_image_canvas.image = img
+        self.original_image_canvas.configure(height=img.height(), width=img.width())
+
+    def change_image_size(self, img, width=400, height=400):
+        return img.resize((width, height), Image.ANTIALIAS)
+
+    def test(self):
+        print('test')
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app = BiometriaGuiApp()
+    app.run()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+
+
