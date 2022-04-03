eeaf845a81ccc06058b88b51bb56978c917f3e1e
mateuszbialowas <mateusz.bialowas2@gmail.com>
2022-04-03 14:34:36 +0200

    add gui segmentation

---
+++ segmentation.py
@@ -0,0 +1,9 @@
+def segmentation(R_value, G_value, B_value, pixel_count, global_mode, fill_mode):
+    # print all values to console
+    print("R: " + str(R_value))
+    print("G: " + str(G_value))
+    print("B: " + str(B_value))
+    print("pixel_count: " + str(pixel_count))
+    print("global_mode: " + str(global_mode))
+    print("fill_mode: " + str(fill_mode))
+    pass
